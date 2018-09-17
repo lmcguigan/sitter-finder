e11d8e59677a25e4c2054f66a7941b531b122ebe
Alba Cisneros <cisnerosan86@gmail.com>
2018-09-16 23:23:22 -0500

    schema and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO customers (customer_name, customer_email, phone, zip_code, time_frame) 
+VALUES ('John Doe','johndoe123@gmail.com',2145825474, 75211,'30 min' );
+
+INSERT INTO customers (customer_name, customer_email, phone, zip_code, time_frame) 
+VALUES ('Marc Winston','mwinston3@gmail.com',2145203214, 75211,'1 hr' );
+
+INSERT INTO jobs(employee_name,employee_ph,employee_email,employee_zip)
+VALUES ('Employee1', 9155227845,'employee1@gmail.com', 75080);
+
+INSERT INTO jobs(employee_name,employee_ph,employee_email,employee_zip)
+VALUES ('Employee2', 9721255445,'employee2@gmail.com', 75211);
