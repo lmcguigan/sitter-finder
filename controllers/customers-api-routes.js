345a51a620136e7418fd1ef86f6b04200f90d16d
oana <oana.fotea@gmail.com>
2018-09-24 19:47:50 -0500

    monday change

--- controllers/customers-api-routes.js
+++ controllers/customers-api-routes.js
@@ -1,141 +1,179 @@
 var router = require('express').Router();
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var db = require('../models');
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    session: true
-  },
-    function(username, password, done) {
-      db.customers.findOne({ username: username }, function (err, customer) {
-        if (err) { return done(err); }
-        if (!customer) { return done(null, false); }
-        if (!customer.verifyPassword(password)) { return done(null, false); }
-        return done(null, customer);
-      });
-    }
-  ));
+//Login Route
+router.post('/api/customers/login',  passport.authenticate('local-login', {
+  successRedirect: '/',
+  failureRedirect: '/',
+  failureFlash: true
+}),
+function(req, res) {
+    console.log("hello");
 
-  passport.use('local-register', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    session: true
-  },
-    function(username, password, done) {
-    //console.log("Hit passport")
-      db.customers.findOne({ where: {name: username} }).then(function (customer) {
-          //console.log("Returned something");
-        //   //console.log(customer);
-        // if (err) { 
-        //     //console.log("Had an error!");
-        //     //console.log(err);
-        //     return done(err); }
-        if (!customer) {
-            //console.log("No customer")
-            return done(null, false); 
-        }
-        // if (!customer.verifyPassword(password)) { 
-        //     //console.log("Bad password")
-        //     return done(null, false); 
-        // }
-        ////console.log("Returning customer");
-        return done(null, customer);
-      });
-    }
-  ));
-
-
-  passport.use('local-login', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    session: true
-  },
-    function(username, password, done) {
-    //console.log("Hit passport")
-      db.customers.find({ where: {email: username, password: password} }).then(function (customer) {
-          ////console.log("Returned something");
-        //   //console.log(customer);
-        // if (err) { 
-        //     //console.log("Had an error!");
-        //     //console.log(err);
-        //     return done(err); }
-        if (!customer) {
-            //console.log("No customer")
-            return done(null, false); 
-        }
-        // if (!customer.verifyPassword(password)) { 
-        //     //console.log("Bad password")
-        //     return done(null, false); 
-        // }
-        //console.log("Returning customer");
-        return done(null, customer);
-      });
-    }
-  ));
-  
-
-
-  
-  passport.serializeUser(function(customer, done) {
-    done(null, customer.id);
-  });
-   
-  passport.deserializeUser(function(id, done) {
-    db.customers.findById({where: {id: id}}, function (err, customer) { 
-        //instead of customer > rows then on next line rows[0]
-        //%%%%//customer or customer.id
-      done(err, customer);
-    });
-  });
-
-    router.post('/api/customers', 
-    passport.authenticate('local-register'), 
-    function(req, res) {
-        ////console.log("Hitting register route")
-        db.customers.create({
-            name: req.body.name,
-            zipcode: req.body.zipcode,
-            email: req.body.email,
-            password: req.body.password,
-            phone: req.body.phone,
-            address: req.body.address,
-        }
-    ).then(function() {
-            res.redirect('/');
-        });
-    });
-
-
-//login authenticate
-router.post('/api/customers/login', 
-passport.authenticate('local-login'), function(req, res) {
-    //console.log("Hitting login route")
-    res.redirect('/');
-    // db.customers.findAll({where: {
-    //     email: req.body.email,
-    //     password: req.body.passport}}).then(function() {
-    //         res.redirect('/');
-    //     });  
+    // if (req.body.remember) {
+    //   req.session.cookie.maxAge = 1000 * 60 * 3;
+    // } else {
+    //   req.session.cookie.expires = false;
+    // }
+res.redirect('/');
 });
 
-  
-    router.get('/api/customers', function(req, res) {
-        db.customers.findAll().then(function(result) {
-            res.json(result);
-        });
-    });
+function isLoggedIn(req, res, next) {
 
-    router.get('/api/customers/name', function(req, res) {
-        db.customers.findOne({ where: {
-            name: req.params.name
-        }
-        }).then(function(result) {
-            res.json(result);
-        });
-    });
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
 
 
-    
-    module.exports = router;
+router.get('/reservations', isLoggedIn, function(req, res) {
+  res.render('manage', {
+    user : req.user // get the user out of session and pass to template
+  });
+});
+
+
+
+//Register Route
+router.post('/api/customers/register',
+
+  function (req, res) {
+    console.log("Hitting register route" + req.body.zipcode)
+
+    db.customers.create({
+      name: req.body.name,
+      zipcode: req.body.zipcode,
+      email: req.body.email,
+      password: req.body.password,
+      phone: req.body.phone,
+      address: req.body.address,
+    }
+    ).then(function (err) {
+      passport.authenticate('local-register', {
+        successRedirect: '/',
+        failureRedirect: '/',
+        failureFlash: true
+      })   
+      res.send({success: true, message: 'You are registered' });
+    }
+    ).catch(function (err) {
+      res.send({success: false, message: err.message });
+    })
+
+  });
+
+//Logout Route
+router.get('/api/customers/logout', function(req, res) {
+  req.logout();
+  //req.flash('success_msg', 'You are logged out');
+  res.redirect('/');
+});
+
+
+// router.get('/register', function (req, res) {
+//   res.render('/', { message: req.flash('registerMessage') })
+// })
+
+
+
+// router.post('/register', function(req, res) {
+//     ////console.log("Hitting register route")
+//     db.customers.findOne({where: {
+//       email: req.body.email}}).then(function(customer) {
+//         if(customer) {
+//           req.flash('error_msg', 'Email already regsitered');
+//         res.redirect('/');
+//         } else {
+//           db.customers.create({
+//             name: req.body.name,
+//             zipcode: req.body.zipcode,
+//             email: req.body.email,
+//             password: req.body.password,
+//             phone: req.body.phone,
+//             address: req.body.address
+//           }).then(function() {
+//             req.flash('success_msg', 'You are now registered and can log in');
+//                 res.redirect('/');
+//           })
+
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(newUser.password, salt, (err, hash) => {
+//     if(err) throw err;
+//     newUser.password = hash;
+//     newUser.save()
+//       .then(user => {
+//         req.flash('success_msg', 'You are now registered and can log in');
+//         res.redirect('/users/login');
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         return;
+//       });
+//   });
+// });
+//         }
+
+
+//       });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+//login Routes
+// router.post('/api/customers/login', 
+// passport.authenticate('local-login', {
+//   successRedirect: '/',
+//   failureRedirect: '/',
+//   failureFlash: true
+// }), function(req, res) {
+//     console.log("Hitting login route")
+//     db.customers.findAll({where: {
+//         email: req.body.email,
+//         password: req.body.passport}}).then(function() {
+//             res.redirect('/');
+//         });  
+// });
+
+// router.get('/api/customers/login', function(req, res) {
+//   res.render('/', {message: req.flash('loginMessage')})
+// })
+
+// router.get('/logout', (req, res) => {
+//   req.logout();
+//   req.flash('success_msg', 'You are logged out');
+//   res.redirect('/users/login');
+// });
+
+
+
+module.exports = router;
+
+    //passport.use(new LocalStrategy({
+      //     usernameField: 'email',
+      //     passwordField: 'password',
+      //     session: true
+      //   },
+      //     function(username, password, done) {
+      //       db.customers.findOne({ username: username }, function (err, customer) {
+      //         if (err) { return done(err); }
+      //         if (!customer) { return done(null, false); }
+      //         if (!customer.verifyPassword(password)) { return done(null, false); }
+      //         return done(null, customer);
+      //       });
+      //     }
+      //   ));
--- server.js
+++ server.js
@@ -2,18 +2,14 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
-var cookieParser = require('cookie-parser');
-
-// db modules
-var db = require("./models");
-
-
+var flash = require('connect-flash');
 
 var PORT = process.env.PORT || 3000;
-// =============================================================
+
 var app = express();
+require('./config/passport')(passport);
+var db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,46 +19,54 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-// Authentication set up with Passport & Express-ssession
-//==============================================================
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: true
-},
-  function(username, password, done) {
-    db.customers.findOne({ username: username }).then(function (customer) {
-      if (!customer) { return done(null, false); }
-      //if (!customer.verifyPassword(password)) { return done(null, false); }
-      return done(customer);
-    });
-  }
-));
+// // Global variables
+// app.use(function(req, res, next){
+//   res.locals.success_msg = req.flash('success_msg');
+//   res.locals.error_msg = req.flash('error_msg');
+//   res.locals.error = req.flash('error');
+//   res.locals.user = req.user || null;
+//   next();
+// });
 
-passport.serializeUser(function(customer, done) {
-  //console.log(customer);
-  done(null, customer);
-});
+// passport.use(new LocalStrategy({
+//   usernameField: 'email',
+//   passwordField: 'password',
+//   session: true
+// },
+//   function(username, password, done) {
+//     db.customers.findOne({ username: username }).then(function (customer) {
+//       if (!customer) { return done(null, false); }
+//       //if (!customer.verifyPassword(password)) { return done(null, false); }
+//       return done(customer);
+//     });
+//   }
+// ));
+
+// passport.serializeUser(function(customer, done) {
+//   //console.log(customer);
+//   done(null, customer);
+// });
  
-passport.deserializeUser(function(user, done) {
-  // done(null, user)
-  //console.log(user);
-  db.customers.findOne({where: {id: user.id}}).then(function (customer) {
-      //%%%%//customer or customer.id
-      //console.log(customer);
+// passport.deserializeUser(function(user, done) {
+//   // done(null, user)
+//   //console.log(user);
+//   db.customers.findOne({where: {id: user.id}}).then(function (customer) {
+//       //%%%%//customer or customer.id
+//       //console.log(customer);
 
-    done(null, customer);
-  });
-});
+//     done(null, customer);
+//   });
+// });
 
 // Routes
 // =============================================================
@@ -75,13 +79,6 @@ app.use(htmlRoutes);
 app.use(customerRoutes);
 app.use(sitterRoutes);
 app.use(registrationRoutes);
-
-//Set up for passport-local-sequelize
-//==============================================================
-// passport.use(db.createStrategy());
- 
-// passport.serializeUser(db.serializeUser());
-// passport.deserializeUser(db.deserializeUser());
 
 // Starts the server to begin listening
 // =============================================================
